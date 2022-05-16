71c2faed5d167339b81ce31a88d9ade50a34d608
Alexey Grebenyuk <vladimir-g2010@mail.ru>
2022-05-16 18:22:49 +0900

    самое важное изменение

--- src/main/java/ru/bgpu/zombies/Live.java
+++ src/main/java/ru/bgpu/zombies/Live.java
@@ -15,7 +15,7 @@ public class Live {
     Image d = new ImageIcon(getClass().getResource("/image/d.png")).getImage();
     
     boolean go = false;
-    int live = 5;
+    int live = 6; //улучшение на свое усмотрение.
     int money = 0;
     
     Font font = new Font(Font.DIALOG, Font.BOLD,30);
